a6a3ad9b0a30d97a0cafa744f4e96eb3cff941e9
unknown <notjonmadden@gmail.com>
2017-07-24 19:53:08 -0400

    build with rollup instead of tsc

--- homebrew/rollup.config.js
+++ homebrew/rollup.config.js
@@ -2,7 +2,7 @@ import resolve from "rollup-plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
 
 export default {
-    entry: "src/index.js",
+    entry: "src/index.ts",
     format: "iife",
     dest: "bundle.js",
     plugins: [
